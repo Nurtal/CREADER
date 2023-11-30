ed19588c7994b7e7acec969571f960cd163a092d
Nurtal <nathan.foulquier.pro@gmail.com>
2023-11-30 09:07:56 +0100

    add sample data & craft chain

--- creader/extractor.py
+++ creader/extractor.py
@@ -1,5 +1,25 @@
-def craft_chain():
-    """ """
+from langchain import LlamaCpp
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import BaseOutputParser
+
+
+def craft_chain(model_path: str):
+    """craft a chain to ..."""
+
+    # load model
+    lllm = LlamaCpp(model_path=model_path)
+
+    # define chat prompt
+    template = "You are helpful assistant that read a text and answer questions about what you read. return only 'Yes' or 'No, nothing else.TEXT:{text}. QUESTION: does the text contains information about {target} ? ANSWER:"
+
+    # assemble chain
+    chat_prompt = ChatPromptTemplate.from_messages([("system", template)])
+
+    # craft chain
+    chain = chat_prompt | lllm
+
+    # return chain
+    return chain
 
 
 def parse_output():
@@ -18,4 +38,12 @@ def extractor():
 
 if __name__ == "__main__":
 
-    extractor()
+    # parameters
+    model_path = "/home/bran/Workspace/misc/llama/models/llama-2-13b-chat.Q5_K_M.gguf"
+    text = "Jimmy est un perroquet qui mesure 65 cm"
+    target = "Size"
+
+    # test
+    chain = craft_chain(model_path)
+    answer = chain.invoke({"text": text, "target": target})
+    print(answer)
